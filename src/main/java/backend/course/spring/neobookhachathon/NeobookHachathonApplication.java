d4b4758d4a7e634aa691c0847bc8f940c13be88d
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-02-22 13:53:20 +0600

    Initial commit

---
+++ src/main/java/backend/course/spring/neobookhachathon/NeobookHachathonApplication.java
@@ -0,0 +1,13 @@
+package backend.course.spring.neobookhachathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobookHachathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeobookHachathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/course/spring/neobookhachathon/NeobookHachathonApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.course.spring.neobookhachathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeobookHachathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
